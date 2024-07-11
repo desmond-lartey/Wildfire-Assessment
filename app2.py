14d656cef89a56ff191e358de348321ec59875cc
Desmond Lartey <larteydesmond3@gmail.com>
2024-07-11 12:57:13 +0200

    v

--- app2.py
+++ app2.py
@@ -102,11 +102,11 @@ def plot_analysis(question1, question2, demo, chart_type, analysis_type, top_n=2
 
 # Main Content
 st.title("South Africa Wildland Fire Survey Analysis")
-#if st.sidebar.button("Plot"):
+if st.sidebar.button("Plot"):
     #plot_analysis(selected_question_1, selected_question_2, selected_demo, selected_chart_type, analysis_type)
 
 # Your existing function definitions
-def plot_analysis(selected_question_1, selected_question_2, selected_demo, selected_chart_type, analysis_type):
+    def plot_analysis(selected_question_1, selected_question_2, selected_demo, selected_chart_type, analysis_type):
     # Your plot analysis code here
     pass
 
